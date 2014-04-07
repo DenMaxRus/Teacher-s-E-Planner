0fe773e6f490f654eb309b317d8f0eddd440c469
DenMaxRus <daniilmaximov2@yandex.ru>
2014-04-07 07:17:44 +0400

    + : финальная, обновлен билд

--- Teacher's E-Planner/gen/com/teacherse_planner/R.java
+++ Teacher's E-Planner/gen/com/teacherse_planner/R.java
@@ -43,15 +43,16 @@ public final class R {
         public static final int navigation_drawer_width=0x7f060002;
     }
     public static final class drawable {
-        public static final int drawer_hint=0x7f020000;
-        public static final int drawer_shadow=0x7f020001;
-        public static final int ic_drawer=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int icon=0x7f020004;
-        public static final int note_additional=0x7f020005;
-        public static final int note_list=0x7f020006;
-        public static final int quad_list=0x7f020007;
-        public static final int wooden_table=0x7f020008;
+        public static final int board=0x7f020000;
+        public static final int drawer_hint=0x7f020001;
+        public static final int drawer_shadow=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int icon=0x7f020005;
+        public static final int note_additional=0x7f020006;
+        public static final int note_list=0x7f020007;
+        public static final int quad_list=0x7f020008;
+        public static final int wooden_table=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0026;
